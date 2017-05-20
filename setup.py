f168e0bb76f260eaf9224cf45452227ab70cd643
Huss <huss@9fin.com>
2017-05-20 13:49:26 +0100

    Enter info into setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='sqlpy',
+  packages=['sqlpy'],
+  version='0.1',
+  description='Write actual SQL to retrieve your data.',
+  author='Huss',
+  author_email='oss@9fin.com',
+  url='https://github.com/9fin/sqlpy',
+  download_url='https://github.com/9fin/sqlpy/archive/0.1.tar.gz',
+  keywords=['sql', 'python', 'postgres'],
+  classifiers=[],
+)
